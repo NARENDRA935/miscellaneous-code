421c85d71b37fe4d970289e3481ce766c3740ad0
Narendra T <narendratarra935@gmail.com>
2024-02-06 19:34:20 +0530

    prometheus code

--- prometheus/main.tf
+++ prometheus/main.tf
@@ -3,23 +3,24 @@ terraform {
     bucket = "d14-terraform"
     key    = "misc/prometheus/terraform.tfstate"
     region = "us-east-1"
+
   }
 }
 
 data "aws_ami" "centos8" {
-  most_recent      = true
-  name_regex       = "Centos-8-DevOps-Practice"
-  owners           = [973714476881]
+  most_recent = true
+  name_regex  = "Centos-8-DevOps-Practice"
+  owners      = ["973714476881"]
 }
 
-
 resource "aws_instance" "prometheus" {
-  ami           = data.aws_ami.centos8.image_id
-  instance_type = "t3.micro"
+  ami                    = data.aws_ami.centos8.image_id
+  instance_type          = "t3.small"
   vpc_security_group_ids = ["sg-0200b43c5a335c1ba"]
 
+
   tags = {
-    name = "prometheus-server"
+    Name = "prometheus-server"
   }
 }
 
@@ -38,3 +39,4 @@ resource "aws_route53_record" "prometheus-public" {
   ttl     = 30
   records = [aws_instance.prometheus.public_ip]
 }
+
